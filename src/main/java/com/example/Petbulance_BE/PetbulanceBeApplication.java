097f0ddf3473d9aa1ca9712a3a51c35163d7cd32
Jina Ham <hamjina61@gmail.com>
2025-09-27 07:27:50 +0900

    initial commit

---
+++ src/main/java/com/example/Petbulance_BE/PetbulanceBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Petbulance_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetbulanceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetbulanceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Petbulance_BE/PetbulanceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Petbulance_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetbulanceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
