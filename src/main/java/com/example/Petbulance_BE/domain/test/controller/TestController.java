7e20538bfaf0bd783d84e060dbcbf8f0297ddc1a
Jina Ham <hamjina61@gmail.com>
2025-09-27 08:44:53 +0900

    [setting] 공통 응답 & 예외처리 설정

---
+++ src/main/java/com/example/Petbulance_BE/domain/test/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.Petbulance_BE.domain.test.controller;
+
+import com.example.Petbulance_BE.domain.test.dto.TestResponseDto;
+import com.example.Petbulance_BE.global.common.error.exception.CustomException;
+import com.example.Petbulance_BE.global.common.error.exception.ErrorCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class TestController {
+    @GetMapping("/health")
+    public TestResponseDto healthCheck() {
+        return new TestResponseDto("테스트에 성공하였습니다!");
+    }
+
+    @GetMapping("/error")
+    public void errorCheck() {
+        throw new CustomException(ErrorCode.TEST_ERROR_CODE);
+    }
+}
---
+++ src/main/java/com/example/Petbulance_BE/domain/test/dto/TestResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.Petbulance_BE.domain.test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestResponseDto {
+    private String message;
+}
