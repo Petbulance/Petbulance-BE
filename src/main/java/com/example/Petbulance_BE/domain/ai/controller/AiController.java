d4624734d29c2249eace0ba511e5f2170826fcc1
kimyounguk1 <kyw020108@gmail.com>
2025-11-29 11:42:34 +0900

    [feat] #2 ai진단 및 응급처치 기능구현

---
+++ src/main/java/com/example/Petbulance_BE/domain/ai/controller/AiController.java
@@ -0,0 +1,30 @@
+package com.example.Petbulance_BE.domain.ai.controller;
+
+import com.example.Petbulance_BE.domain.ai.dto.res.DiagnosisResDto;
+import com.example.Petbulance_BE.domain.ai.service.AiService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ai")
+@Slf4j
+@RequiredArgsConstructor
+public class AiController {
+
+    private final AiService aiService;
+
+    @PostMapping("/diagnosis")
+    public Mono<DiagnosisResDto> aiDiagnosis(@RequestParam(name = "images", required = false) List<MultipartFile> images,
+    @RequestParam(name = "animalType")String animalType, @RequestParam(name = "symptom")String symptom) {
+        return aiService.aiDiagnosisProcess(images, animalType, symptom);
+    }
+
+}
