3405a40facb75db7d17b2dec2ae6f02e8aaadf84
kimyounguk1 <kyw020108@gmail.com>
2025-12-10 13:52:43 +0900

    [feat] #76 람다 트리거를 통한 이미지 리사이징 기능 구현

--- src/main/java/com/example/Petbulance_BE/global/common/s3/S3Service.java
+++ src/main/java/com/example/Petbulance_BE/global/common/s3/S3Service.java
@@ -78,7 +78,7 @@ public class S3Service {
     public boolean doesObjectExist(String key) {
         try{
             HeadObjectRequest headReq = HeadObjectRequest.builder()
-                    .bucket(bucket)
+                    .bucket(bucket+"-resized")
                     .key(key)
                     .build();
             s3.headObject(headReq);
@@ -104,7 +104,7 @@ public class S3Service {
 
     //객체 조회 URL
     public String getObject(String key) {
-        return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + key;
+        return "https://" + bucket +"-resized" + ".s3." + region + ".amazonaws.com/" + key;
     }
 
 
