195bba461807ebec1c51ced703a0dbfd7b855806
Jina Ham <hamjina61@gmail.com>
2025-12-26 15:45:06 +0900

    [deploy] #112 파일 설정 수정

--- src/main/java/com/example/Petbulance_BE/global/firebase/FirebaseInitializer.java
+++ src/main/java/com/example/Petbulance_BE/global/firebase/FirebaseInitializer.java
@@ -1,25 +1,28 @@
 package com.example.Petbulance_BE.global.firebase;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.context.annotation.Profile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Profile("prod")
 public class FirebaseInitializer {
+
     public static void initialize() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/petbulance-b316f-firebase-adminsdk-fbsvc-8c92a7aab5.json");
-//      /home/ubuntu/Petbulance-BE/src/main/resources/petbulance-b316f-firebase-adminsdk-fbsvc-8c92a7aab5.json
-        //src/main/resources/petbulance-b316f-firebase-adminsdk-fbsvc-8c92a7aab5.json
+
+        InputStream serviceAccount =
+                new ClassPathResource("petbulance-b316f-firebase-adminsdk-fbsvc-8c92a7aab5.json")
+                        .getInputStream();
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
-        if(FirebaseApp.getApps().isEmpty()){
+        if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
         }
     }
