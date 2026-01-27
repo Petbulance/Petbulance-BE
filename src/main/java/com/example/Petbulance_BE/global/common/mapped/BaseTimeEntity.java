0752627682b26d4b7fe5020c641c3570213150c6
kimyounguk1 <130636997+kimyounguk1@users.noreply.github.com>
2026-01-28 00:41:06 +0900

    Update BaseTimeEntity.java

--- src/main/java/com/example/Petbulance_BE/global/common/mapped/BaseTimeEntity.java
+++ src/main/java/com/example/Petbulance_BE/global/common/mapped/BaseTimeEntity.java
@@ -1,15 +1,12 @@
 package com.example.Petbulance_BE.global.common.mapped;
-import lombok.experimental.SuperBuilder;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@Builder
 @Getter
 public class BaseTimeEntity {
     @CreationTimestamp
