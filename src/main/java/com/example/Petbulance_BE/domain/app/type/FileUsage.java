9f6141910a025d459dedd03df78b2fa62fd74fff
Jina Ham <hamjina61@gmail.com>
2026-02-11 15:36:10 +0900

    [fix] #279 presigned url 발급시 TEST용 ENUM 추가

--- src/main/java/com/example/Petbulance_BE/domain/app/type/FileUsage.java
+++ src/main/java/com/example/Petbulance_BE/domain/app/type/FileUsage.java
@@ -3,5 +3,11 @@ package com.example.Petbulance_BE.domain.app.type;
 public enum FileUsage {
     NOTICE_FILE,
     BANNER,
-    POST
+    POST,
+    COMMENT,
+
+    TEST_NOTICE_FILE,
+    TEST_BANNER,
+    TEST_POST,
+    TEST_COMMENT
 }
